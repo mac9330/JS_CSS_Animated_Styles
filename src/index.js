161dfe34ba5f9a3f2348a247676e828f348368a5
Mac9330 <macyoung94@gmail.com>
2020-08-18 19:20:57 -0400

    add background animation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import "./styles/index.scss";
 let scrollTop = window.scrollY
 
 const heroEles = document.querySelectorAll('.hero')
-heroEles[0].style.left = "50%"
-heroEles[1].style.left = "50%"
+heroEles[0].style.left = "42%"
+heroEles[1].style.left = "42%"
 heroEles[0].style.top = scrollTop + 200 + "px"
 heroEles[1].style.top = scrollTop + 300 + "px"
 
@@ -12,13 +12,13 @@ heroEles[1].style.top = scrollTop + 300 + "px"
 const scrollHero = () => {
   let scrollTop = window.scrollY - (prevScrollTop || 0);
 
-  heroEles[0].style.left =  50 - scrollTop / 8 + "%"
+  heroEles[0].style.left =  42 - scrollTop / 8 + "%"
   heroEles[0].style.top = scrollTop + 200 + "px"
 
-  heroEles[1].style.left =  50 + scrollTop / 8 + "%"
+  heroEles[1].style.left =  42 + scrollTop / 8 + "%"
   heroEles[1].style.top = scrollTop + 300 + "px"
   
-  let prevScrollTop = window.scrollY // important being used on line 18 dont delete again :(
+  let prevScrollTop = window.scrollY // important being used on line 13 dont delete again :(
 }
 
 
